b8ca298ed711d919d17ceb2504cffcb0e85f8df6
Romina Degan <r.degan.20@abdn.ac.uk>
2024-04-28 17:07:41 +0100

    Added Comments

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const TeachableMachine = require("@sashido/teachablemachine-node");
 
 const model = new TeachableMachine({
-  modelUrl: "https://teachablemachine.withgoogle.com/models/r6BBk-hiN/"
+  modelUrl: "https://teachablemachine.withgoogle.com/models/r6BBk-hiN/" //Replace this link with your own generated link from the website
 });
 
 model.classify({
-  imageUrl: "https://www.loveyourdog.com/wp-content/uploads/2019/08/Brindle-American-Bulldog-1344x746.jpg",
+  imageUrl: "https://www.loveyourdog.com/wp-content/uploads/2019/08/Brindle-American-Bulldog-1344x746.jpg", //Choose another random image from the internet, make sure its in the format of a URL (if you are unsure ask one of us!!)
 }).then((predictions) => {
   console.log("Predictions:", predictions);
 }).catch((e) => {
